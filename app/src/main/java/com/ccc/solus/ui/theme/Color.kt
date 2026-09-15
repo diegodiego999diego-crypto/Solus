499a157efc29c55d36920595c0b7b8996c110b3a
diegodiego999diego-crypto <diego.diego.999.diego@gmail.com>
2026-09-15 10:00:33 -0600

    Update Color.kt

--- app/src/main/java/com/ccc/solus/ui/theme/Color.kt
+++ app/src/main/java/com/ccc/solus/ui/theme/Color.kt
@@ -1,1 +1,23 @@
-// TODO
+package com.ccc.solus.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Azules principales
+val Blue40 = Color(0xFF1565C0)
+val Blue80 = Color(0xFF90CAF9)
+val BlueGrey40 = Color(0xFF455A64)
+val BlueGrey80 = Color(0xFFB0BEC5)
+
+// Acentos
+val Cyan40 = Color(0xFF00838F)
+val Cyan80 = Color(0xFF80DEEA)
+
+// Superficies claras
+val SurfaceLight = Color(0xFFF5F8FF)
+val SurfaceVariantLight = Color(0xFFDDE3F0)
+val OnSurfaceLight = Color(0xFF1A1C1E)
+
+// Superficies oscuras
+val SurfaceDark = Color(0xFF101418)
+val SurfaceVariantDark = Color(0xFF3A4048)
+val OnSurfaceDark = Color(0xFFE2E2E6)
