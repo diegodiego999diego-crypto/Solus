0eae744eaaabd110b08db9ca491647b73dc8916d
diegodiego999diego-crypto <diego.diego.999.diego@gmail.com>
2026-09-15 09:45:32 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,5 @@
-// TODO
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
