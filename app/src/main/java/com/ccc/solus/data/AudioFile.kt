698ee961b16aea8c3d0fa03764d313b32eab3fed
diegodiego999diego-crypto <diego.diego.999.diego@gmail.com>
2026-09-15 10:05:27 -0600

    Update AudioFile.kt

--- app/src/main/java/com/ccc/solus/data/AudioFile.kt
+++ app/src/main/java/com/ccc/solus/data/AudioFile.kt
@@ -1,1 +1,17 @@
-// TODO
+package com.ccc.solus.data
+
+import java.io.File
+
+data class AudioFile(
+    val file: File,
+    val name: String,
+    val path: String,
+    val size: Long,
+    val lastModified: Long
+) {
+    val extension: String
+        get() = file.extension.lowercase()
+
+    val displayName: String
+        get() = file.nameWithoutExtension
+}
