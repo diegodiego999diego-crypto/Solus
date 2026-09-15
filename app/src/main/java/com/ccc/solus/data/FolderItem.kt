e738ad0c9bdfa1cbd0d217b40a0a8e1fb2a6f482
diegodiego999diego-crypto <diego.diego.999.diego@gmail.com>
2026-09-15 10:05:52 -0600

    Update FolderItem.kt

--- app/src/main/java/com/ccc/solus/data/FolderItem.kt
+++ app/src/main/java/com/ccc/solus/data/FolderItem.kt
@@ -1,1 +1,13 @@
-// TODO
+package com.ccc.solus.data
+
+import java.io.File
+
+data class FolderItem(
+    val folder: File,
+    val name: String,
+    val path: String,
+    val audioCount: Int
+) {
+    val displayName: String
+        get() = name.ifBlank { folder.absolutePath }
+}
